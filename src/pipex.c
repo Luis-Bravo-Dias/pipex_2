0f79d6d52fb0cd7f2745c46b3982dc2214efafa3
Luis miguel Leiria bravo rodrigues dias <lleiria-@c2r10s7.42lisboa.com>
2022-09-06 12:56:08 +0100

    5

--- src/pipex.c
+++ src/pipex.c
@@ -6,17 +6,38 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 15:08:42 by lleiria-          #+#    #+#             */
-/*   Updated: 2022/09/05 15:32:01 by lleiria-         ###   ########.fr       */
+/*   Updated: 2022/09/06 12:42:08 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-void	pipex()
+void	pipex(char **av, char **env)
 {
-	pid_t	pid;
+	pid_t	child1;
+	pid_t	child2;
 	int		end[2];
+	int		fd_in;
+	int 	fd_out;
+	char	*cmd1_path;
+	char	*cmd2_path;
 
 	if (pipe(ends) == -1)
 		return (ERROR);
+	child1 = fork ();
+	if (child1 == -1)
+		return (ERROR);
+	if (child1 == 0)
+	{
+		fd_in = open(infile, O_RDONLY);
+		if (!fd_in)
+			return (ERROR);
+		if (dup2(fd_in, STDIN_FILENO) == -1)
+			return (ERROR);
+		if (dup2(end[1], STDOUT_FILENO) == -1)
+			return (ERROR);	
+		cmd1_path = get_cmd(av[2], env);
+		execve(cmd1_path, av[2], env);
+	}
+		
 }
